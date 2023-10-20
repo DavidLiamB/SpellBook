41c80113fb4224b01800f0214524f78c79329b44
Liam Ball <davidliamb38@gmail.com>
2023-10-20 10:33:51 -0600

    first commit

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'liamlogin.us.auth0.com'
+export const audience = 'https://Liamthestudent.com'
+export const clientId = 'krU2ZyvH8j9MfjtKiqefSdLAftyTVZvJ'
