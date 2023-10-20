dcdfebfce06841455e6e223863ec18f22847930c
Daniel Palomarez <daniel.palomarez5@gmail.com>
2023-10-20 11:11:07 -0600

    you can now make posts slow pokes

--- server/src/controllers/PostsController.js
+++ server/src/controllers/PostsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { postService } from "../services/PostsService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,6 +7,8 @@ export class PostsController extends BaseController {
         super('api/posts')
         this.router
             .get('', this.getAllPosts)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createPost)
     }
 
     async getAllPosts(req, res, next) {
@@ -15,7 +18,18 @@ export class PostsController extends BaseController {
         } catch (error) {
             next(error)
         }
+    }
 
+    async createPost(req, res, next) {
+        try {
+            const body = req.body
+            const userId = req.userInfo.id
+            body.creatorId = userId
+            const newPost = await postService.createPost(body)
+            res.send(newPost)
+        } catch (error) {
+            next(error)
+        }
     }
 
 }
